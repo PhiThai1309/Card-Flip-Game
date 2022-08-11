01d38bb4cd69378049d251896def292f434eedff
Phi Thai <s3878070@rmit.edu.vn>
2022-08-11 15:35:33 +0700

    Initial Commit

---
+++ Card Flip Game/Card_Flip_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Flip_GameApp.swift
+//  Card Flip Game
+//
+//  Created by Phi Thai on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Card_Flip_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
